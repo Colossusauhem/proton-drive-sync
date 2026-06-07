2489af1ef9a32e0c6b1a1138b956c5196952bb1b
Colossusauhem <begumavan31529@gmail.com>
2026-06-07 15:41:21 -0500

    Create src/proton/index.ts

---
+++ src/proton/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Proton Drive API
+ *
+ * Re-exports all Proton Drive API functions and types.
+ */
+
+export { createNode } from './create.js';
+export { deleteNode } from './delete.js';
+export { relocateNode, getParentFolderUid } from './rename.js';
+export {
+  parsePath,
+  findNodeByName,
+  findFileByName,
+  findFolderByName,
+  traverseRemotePath,
+  formatSize,
+} from './utils.js';
+export type {
+  NodeData,
+  NodeResult,
+  RootFolderResult,
+  CreateFolderResult,
+  DeleteResult,
+  UploadController,
+  FileUploader,
+  FileRevisionUploader,
+  UploadMetadata,
+  BaseProtonDriveClient,
+  CreateProtonDriveClient,
+  DeleteProtonDriveClient,
+  ProtonDriveClient,
+  CreateResult,
+  DeleteOperationResult,
+  ParsedPath,
+  ApiError,
+} from './types.js';
+export type { RelocateResult } from './rename.js';
